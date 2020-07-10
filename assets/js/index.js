028df267577a6c800e425806b80a341d94b80abd
Hasala2002 <hasala.h@hotmail.com>
2020-07-10 15:27:14 +0530

    fixed responsive all issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,44 +1,18 @@
-const carousel = document.getElementById('carousel');
-const slider = document.getElementById('slider');
-
-const next = document.getElementById('next');
-const prev = document.getElementById('prev');
-let direction;
-
-function next(){
-  direction = -1;
-  carousel.style.justifyContent = 'flex-start';
-  slider.style.transform = 'translate(-20%)';  
-  alert('heello');
+function alertmsg(){
+  alert('hello!');
 }
 
-function prev(){
-  if (direction === -1) {
-    direction = 1;
-    slider.appendChild(slider.firstElementChild);
-  }
-  carousel.style.justifyContent = 'flex-end';    
-  slider.style.transform = 'translate(20%)';  
-  
-}
+navdrop =true;
 
-slider.addEventListener('transitionend', function() {
-  // get the last element and append it to the front
-  
-  if (direction === 1) {
-    slider.prepend(slider.lastElementChild);
-  } else {
-    slider.appendChild(slider.firstElementChild);
+function navbardrop(){
+  if (navdrop){
+  hiddenav= document.getElementById('navbar2');
+  hiddenav.style.height="400px";
+  navdrop=false;
   }
-  
-  slider.style.transition = 'none';
-  slider.style.transform = 'translate(0)';
-  setTimeout(() => {
-    slider.style.transition = 'all 0.5s';
-  })
-}, false);
-  
-
-function test(){
-    alert('hello');
+  else{
+    hiddenav= document.getElementById('navbar2');
+    hiddenav.style.height="0px";
+    navdrop =true;
+  }
 }
